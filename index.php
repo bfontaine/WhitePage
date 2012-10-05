4d70110bd0c4b829b8a344c6f96325604ce0a1e9
Baptiste Fontaine <batifon@yahoo.fr>
2012-10-05 12:00:14 +0200

    index page added.

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+### Configuration
+    $lang    = 'en';
+    $charset = 'utf-8';
+    $title   = 'White Page';
+###
+?>
+<!doctype html>
+<html lang="<?php echo $lang; ?>" dir="ltr">
+        <head>
+            <meta charset="<?php echo $charset ?>" />
+            <meta name="language" content="<?php echo $lang; ?>" />
+            <title><?php echo $title; ?></title>
+
+            <meta name="robots" content="noindex,nofollow" />
+            <link rel="stylesheet" href="whitepage.css" media="all" />
+
+        </head>
+        <body>
+            <article id="whitepage"></article>
+            <script src="jquery-1.8.2.min.js"></script>
+            <script src="whitepage.js"></script>
+        </body>
+</html>
+

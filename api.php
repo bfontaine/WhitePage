114311afab8c062b6f0df57633621ac225ff107d
Baptiste Fontaine <batifon@yahoo.fr>
2012-10-05 11:45:32 +0200

    API functions added.

--- api.php
+++ api.php
@@ -1,3 +1,45 @@
 <?php
 
+function response($data=null, $err=null) {
+    $resp = array();
+    
+    if ($data)
+        $resp['data'] = $data;
+
+    if ($err)
+        $resp['error'] = $err;
+    
+    echo json_encode($resp);
+}
+
+function save($filename) {
+    if (!isset($_POST['text']))
+        return response();
+
+    $text = trim($_POST['text']);
+
+    if (!file_put_contents($filename, $text))
+        return response(null, 'Writing error.')
+
+    return response($text);
+}
+
+function retrieve($filename) {
+    $text = file_get_contents($filename);
+
+    return response($text);
+}
+
+function call($filename) {
+    if (isset($_POST['save']) && $_POST['save'])
+        return save($filename);
+
+    if (isset($_GET['retrieve'] && $_GET['retrieve']))
+        return retrieve($filename);
+
+    return response(null, 'unrecognized action.');
+}
+
+call('save.txt');
+
 ?>
